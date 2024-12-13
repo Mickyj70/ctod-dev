74d03e0b51b23267a125d62bf840f6b179b452e8
Michael <michael.o.joseph777@gmail.com>
2024-12-13 07:28:26 +0100

    added icons

--- app/holdings/page.jsx
+++ app/holdings/page.jsx
@@ -1,24 +1,26 @@
-"use client"
+"use client";
 import React from "react";
 import { RxMixerHorizontal } from "react-icons/rx";
 import { RefreshCw } from "lucide-react";
-import { Table } from "../../components/ui/table/table"
+import { Table } from "../../components/ui/table/table";
 import HoldingTokenTable from "../../components/ui/LockedTokenTable/HoldingsTokenTable";
+import { CiSearch } from "react-icons/ci";
+
 export default function Page() {
   const handleFavoriteToggle = ({ index, isFavorited }) => {
-    console.log(`Token ${index} is ${isFavorited ? "favorited" : "unfavorited"}`);
+    console.log(
+      `Token ${index} is ${isFavorited ? "favorited" : "unfavorited"}`
+    );
   };
 
   const formatPrice = (price) => {
     if (price >= 1000000) {
-      return (price / 1000000) + 'M';
+      return price / 1000000 + "M";
     } else if (price >= 1000) {
-      return (price / 1000) + 'k';
+      return price / 1000 + "k";
     }
     return price.toString();
   };
-
-
 
   const token = [
     {
@@ -31,7 +33,6 @@ export default function Page() {
       remaining: "10M",
       sold: "10k",
       changeInPL: "1%",
-
     },
   ];
 
@@ -39,80 +40,91 @@ export default function Page() {
     console.log(`Quick buy ${token.name}`);
   };
 
-
   return (
     <div className="Container">
       <div className="innerContainer">
         {/* header */}
         <div className="">
-          <p className="header flex items-center gap-x-3">PORTFOLIO <span className="text-primaryText text-sm">SHOW HIDDEN <span>switch</span> </span></p>
-
+          <p className="flex items-center header gap-x-3">
+            PORTFOLIO{" "}
+            <span className="text-sm text-primaryText">
+              SHOW HIDDEN <span>switch</span>{" "}
+            </span>
+          </p>
         </div>
 
-        <div className="flex justify-between items-center w-full ">
+        <div className="flex items-center justify-between w-full ">
           <div className="flex items-center gap-x-3">
-            <div className="primarybutton flex-col px-6 gap-y-1">
+            <div className="flex-col px-6 primarybutton gap-y-1">
               <p>INVESTED</p>
-              <p className="w-full flex items-center gap-x-1"> <span className="text-xs">icon</span> 100k</p>
+              <p className="flex items-center w-full gap-x-1">
+                {" "}
+                <span className="text-xs">icon</span> 100k
+              </p>
             </div>
-            <div className="primarybutton flex-col px-6 gap-y-1">
+            <div className="flex-col px-6 primarybutton gap-y-1">
               <p>REMAINING</p>
-              <p className="w-full flex items-center gap-x-1"> <span className="text-xs">icon</span> 50k</p>
+              <p className="flex items-center w-full gap-x-1">
+                {" "}
+                <span className="text-xs">icon</span> 50k
+              </p>
             </div>
-            <div className="primarybutton flex-col px-6 gap-y-1">
+            <div className="flex-col px-6 primarybutton gap-y-1">
               <p>SOLD</p>
-              <p className="w-full flex items-center gap-x-1"> <span className="text-xs">icon</span> 50k</p>
+              <p className="flex items-center w-full gap-x-1">
+                {" "}
+                <span className="text-xs">icon</span> 50k
+              </p>
             </div>
-            <div className="primarybutton flex-col px-6 gap-y-1">
+            <div className="flex-col px-6 primarybutton gap-y-1">
               <p>CHANGE IN P&L</p>
               <p className="w-full text-positive">{"10.04%"}</p>
             </div>
-
           </div>
           <div className="flex items-center gap-x-2">
             <div className="primarybutton">
-
               <RefreshCw size={18} />
-              <p className="capitalize" >Refresh P&L</p>
+              <p className="capitalize">Refresh P&L</p>
             </div>
 
             <p className="text-sm">Last refresh 1m ago</p>
           </div>
         </div>
 
-
-        <div className="flex justify-between items-center w-full ">
+        <div className="flex items-center justify-between w-full ">
           <div className="flex items-center gap-x-3 w-fit">
-            <div className="primarybutton h-12">
+            <div className="h-12 primarybutton">
               <RxMixerHorizontal size={20} />
               <p>filters</p>
             </div>
 
-            <div className="w-full h-12 bg-sidebar border border-bordercolor rounded-md py-2 px-3 flex items-center gap-x-3">
-              <p>icon</p>
+            <div className="flex items-center w-full h-12 px-3 py-2 border rounded-md bg-sidebar border-bordercolor gap-x-3">
+              <p>
+                <CiSearch />
+              </p>
               <input
                 type="text"
                 placeholder="SEARCH TOKEN "
-                className="bg-sidebar w-full h-full outline-none font-bold placeholder:text-placeholderText uppercase"
+                className="w-full h-full font-bold uppercase outline-none bg-sidebar placeholder:text-placeholderText"
               />
             </div>
           </div>
 
-
           <div className="flex items-center gap-x-3 ">
-            <div className="w-full h-12 bg-sidebar border border-bordercolor rounded-md py-2 px-3 flex items-center gap-x-3">
-              <p>icon</p>
+            <div className="flex items-center w-full h-12 px-3 py-2 border rounded-md bg-sidebar border-bordercolor gap-x-3">
+              <p>
+                <CiSearch />
+              </p>
               <input
                 type="text"
                 placeholder="SEARCH TOKEN "
-                className="bg-sidebar w-full h-full outline-none font-bold placeholder:text-placeholderText uppercase"
+                className="w-full h-full font-bold uppercase outline-none bg-sidebar placeholder:text-placeholderText"
               />
             </div>
 
-            <div className="primarybutton h-12 w-8/12 ">
-
+            <div className="w-8/12 h-12 primarybutton ">
               <RefreshCw size={18} />
-              <p className="capitalize" >Refresh P&L</p>
+              <p className="capitalize">Refresh P&L</p>
             </div>
           </div>
         </div>
@@ -129,7 +141,6 @@ export default function Page() {
 
           <HoldingTokenTable tokens={token} />
         </div>
-
       </div>
     </div>
   );
